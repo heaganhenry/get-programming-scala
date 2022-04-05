018b9e77eeedbd7a03e3b7a09c77ebcb5855d952
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-04-05 15:34:11 -0400

    Complete lesson 6

---
+++ src/main/scala/L6-PowFunction.scala
@@ -0,0 +1,14 @@
+// Define a function pow that takes two parameters of type Int, and that returns an Int. Call the first parameter exponent and the second one base with a default of two.
+// The function should calculate the power of a number as follows: if the parameter exponent has a value of three and base two, it should compute 2^3. 
+// Implement the function using a loop, without using the function Math.pow.
+
+object LessonSix {
+    def pow(exponent: Int, base: Int = 2): Int = {
+        var result = 1
+        for (e <- 1 to exponent) {
+            result *= base
+        }
+
+        result
+    }
+}
