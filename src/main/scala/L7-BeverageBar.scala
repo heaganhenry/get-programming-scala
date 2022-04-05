143eed443bf42f17cf38a9d0a6ed1c383e5df4e7
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-04-05 17:34:32 -0400

    Complete lesson 7

---
+++ src/main/scala/L7-BeverageBar.scala
@@ -0,0 +1,14 @@
+// A bar serves cold and hot beverages. You can request to add more ice to a cold drink or to reheat an hot one.
+// Express these relations using classes and subclasses.
+
+object LessonSeven {
+    abstract class Drink(name: String, price: Double) 
+
+    class HotDrink(name: String, price: Double) extends Drink(name, price) {
+        def reheat() = ???
+    }
+
+    class ColdDrink(name: String, price: Double) extends Drink(name, price) {
+        def addIce(cubes: Int) = ???
+    }
+}
