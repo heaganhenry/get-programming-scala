b97d6c9127e6b0abd43acbbbc3480730724a93a4
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-06-01 05:10:46 -0400

    Refactor lesson05

--- unit1/lesson05/src/main/scala/L5-ApplyDiscount.scala
+++ unit1/lesson05/src/main/scala/L5-ApplyDiscount.scala
@@ -1,16 +1,14 @@
 // Write a program to compute the discount for a given price as follows:
-// 0% discount if the price is less than $50
-// 10% discount if the price is at least $50 but less than $100
-// 15% discount if the price is at least $100
+// 15% discount if the price is at least $100, 10% discount if the price is at least $50 but less than $100, 0% discount if the price is less than $50
 
 object LessonFive {
     def applyDiscount(price: Double): Double = {
         val discount = {
-            if (price < 50) 1
-            else if (price >= 50 && price < 100) 0.90
-            else 0.85
+            if (price >= 100) 0.15
+            else if (price >= 50) 10
+            else 0
         }
 
-        price * discount
+        price * (1 - discount)
     }
 }
